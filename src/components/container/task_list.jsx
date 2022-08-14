92419833dea6361de7d973562265f27920e33fd7
Antonio <antoniopdspain@gmail.com>
2022-08-14 18:22:38 +0200

    Componented Forms con useState

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -8,10 +8,12 @@ const TaskListComponent = () => {
 
     // Estado del componente
     const [tasks, setTasks] = useState([defaultTask]);
+    const [loading, setLoading] = useState(true);
 
     // Control del ciclo de vida del componente
     useEffect(() => {
         console.log('Task State has been modified');
+        setLoading(false);
         return () => {
             console.log('Task list component is going to unmount');
         };
